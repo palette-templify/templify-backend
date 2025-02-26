836f46ef222510631a5a56499c064bdd76bd6025
김성재 <sngjae972@gmail.com>
2025-02-26 18:14:04 +0900

    refactor: 미사용 import 제거

--- history/src/main/java/com/ktds/templify/history/controller/HistoryController.java
+++ history/src/main/java/com/ktds/templify/history/controller/HistoryController.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
