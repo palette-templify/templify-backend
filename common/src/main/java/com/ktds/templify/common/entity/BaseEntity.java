05f975ca53dcf5cca12f90faec30d823e388f9a3
김성재 <sngjae972@gmail.com>
2025-02-26 17:12:05 +0900

    fix: BaseEntity에 @SuperBuilder 어노테이션 추가
    BaseEntity를 상속받은 클래스에 대한 builder 패턴 사용 시 BaseEntity의 컬럼을 사용할 수 있도록 조치

--- common/src/main/java/com/ktds/templify/common/entity/BaseEntity.java
+++ common/src/main/java/com/ktds/templify/common/entity/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -17,6 +18,7 @@ import java.time.LocalDateTime;
  */
 @Getter
 @MappedSuperclass
+@SuperBuilder
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     
