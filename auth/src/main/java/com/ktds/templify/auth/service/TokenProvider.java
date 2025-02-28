67bc01eff66bcc240438913238a9d37a95e00720
Donggi Yang <xronace@gmail.com>
2025-02-28 14:34:43 +0900

    (Trello: yOKHeexg) refactor: SecretKey를 사용하도록 변경

--- auth/src/main/java/com/ktds/templify/auth/service/TokenProvider.java
+++ auth/src/main/java/com/ktds/templify/auth/service/TokenProvider.java
@@ -3,12 +3,15 @@ package com.ktds.templify.auth.service;
 import com.ktds.templify.auth.dto.TokenResponse;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Slf4j
@@ -28,21 +31,23 @@ public class TokenProvider {
 
         long userId = Long.parseLong(authentication.getName());
 
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
         // JWT 토큰 생성 로직
         String accessToken = Jwts.builder()
             .setSubject("accessToken")
             .claim("userId", userId)
             .setIssuedAt(now)
             .setExpiration(accessExpiryDate)
-            .signWith(SignatureAlgorithm.HS512, secret)
+            .signWith(key, SignatureAlgorithm.HS512)
             .compact();
             
-        String refreshToken = createRefreshToken(userId);
+        String refreshToken = createRefreshToken(userId, key);
         
         return new TokenResponse(accessToken, refreshToken);
     }
     
-    private String createRefreshToken(Long userId) {
+    private String createRefreshToken(Long userId, SecretKey key) {
         Date now = new Date();
         Date refreshExpiryDate = new Date(now.getTime() + (expiration * 7));
 
@@ -52,7 +57,7 @@ public class TokenProvider {
             .claim("userId", userId)
             .setIssuedAt(now)
             .setExpiration(refreshExpiryDate)
-            .signWith(SignatureAlgorithm.HS512, secret)
+            .signWith(key, SignatureAlgorithm.HS512)
             .compact();
     }
 }
