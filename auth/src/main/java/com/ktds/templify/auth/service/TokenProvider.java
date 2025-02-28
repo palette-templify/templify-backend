24bd7837027d3b05e9dc6dbf3508c31de56dba9d
Donggi Yang <xronace@gmail.com>
2025-02-28 11:51:02 +0900

    (Trello: yOKHeexg) feat: expiration 추가 및 설정 파일에서 설정값 로딩이 가능하도록 @Value 어노테이션 추가

--- auth/src/main/java/com/ktds/templify/auth/service/TokenProvider.java
+++ auth/src/main/java/com/ktds/templify/auth/service/TokenProvider.java
@@ -3,20 +3,31 @@ package com.ktds.templify.auth.service;
 import com.ktds.templify.auth.dto.TokenResponse;
 import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class TokenProvider {
     
-    private final String secret;
-    private final long expiration;
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
     
     public TokenResponse createToken(Authentication authentication) {
+        Date now = new Date();
+        Date accessExpiryDate = new Date(now.getTime() + expiration);
+
         // JWT 토큰 생성 로직
         String accessToken = Jwts.builder()
             .setSubject(authentication.getName())
+            .setIssuedAt(now)
+            .setExpiration(accessExpiryDate)
             .signWith(SignatureAlgorithm.HS512, secret)
             .compact();
             
@@ -26,9 +37,14 @@ public class TokenProvider {
     }
     
     private String createRefreshToken(String userId) {
+        Date now = new Date();
+        Date refreshExpiryDate = new Date(now.getTime() + (expiration * 7));
+
         // Refresh 토큰 생성 로직
         return Jwts.builder()
             .setSubject(userId)
+            .setIssuedAt(now)
+            .setExpiration(refreshExpiryDate)
             .signWith(SignatureAlgorithm.HS512, secret)
             .compact();
     }
